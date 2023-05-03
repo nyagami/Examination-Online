926e302ee229a4000417af51ce9abc7306f486e7
Nyagami <hoangquan05112002@gmail.com>
2023-05-03 16:15:47 +0700

    init Examination and Question Entity

--- src/main/java/main/models/Examination.java
+++ src/main/java/main/models/Examination.java
@@ -1,4 +1,35 @@
 package main.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Examination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title = "Chưa đặt tên";
+    private String description = "Bài kiểm tra";
+    @ManyToMany(targetEntity = Question.class)
+    private List<Question> questions;
+    @NotNull(message = "Hãy thêm ngày bắt đầu")
+    @Column(nullable = false)
+    private Date startDate;
+    @NotNull(message = "Hãy thêm ngày kết thúc")
+    @Column(nullable = false)
+    private Date endDate;
+
+    @NotNull
+    private Long totalTime = (long) 60; // in seconds
+    @Column(nullable = false)
+    @NotNull(message = "Bài kiểm tra phải có điểm")
+    private Float points;
+    private boolean isVisible = false;
 }
--- src/main/java/main/models/Question.java
+++ src/main/java/main/models/Question.java
@@ -1,4 +1,26 @@
 package main.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @NotNull(message = "Cần phải có đề bài")
+    private String description;
+    @Column(length = 410)
+    @NotNull
+    private String answer;  // split by &&&
+    @Max(value = 3, message = "Không có đáp án nào như vậy")
+    @NotNull(message = "Câu hỏi cần có câu trả lời đúng")
+    private Integer correctAnswer;
+
 }
