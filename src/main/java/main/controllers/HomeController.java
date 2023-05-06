4f87feb2664bed00af37984170ff113d3cedee33
Nyagami <hoangquan05112002@gmail.com>
2023-05-06 23:16:16 +0700

    add spring security

--- src/main/java/main/controllers/HomeController.java
+++ src/main/java/main/controllers/HomeController.java
@@ -2,18 +2,11 @@ package main.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String home(@RequestParam(value = "role",required = false) String role){
-        if(role.equals("student")){
-            return "homestudent";
-        }
-        if(role.equals("teacher")){
-            return "hometeacher";
-        }
+    public String home(){
         return "home";
     }
 
