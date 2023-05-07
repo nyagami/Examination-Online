db877356b0ce635e09916ed281347c8e282e739f
manh1357902 <115218176+manh1357902@users.noreply.github.com>
2023-05-07 22:26:13 +0700

    spring security 1 (#15)

--- src/main/java/main/controllers/LoginController.java
+++ src/main/java/main/controllers/LoginController.java
@@ -1,51 +1,27 @@
 package main.controllers;
 
-import jakarta.servlet.http.HttpSession;
-import main.data.UserRepository;
-import main.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @GetMapping("/login")
-    public String login(@RequestParam(value = "error",required = false) String error, Model model){
-    	if(error!=null) {
-	        if(error.equals("password")){
-	            model.addAttribute("error","Mật khẩu của bạn không đúng");
-	        }else if(error.equals("email")){
-	            model.addAttribute("error","Tài khoản không tồn tại");
-	        }
-    	}
-        return "login";
-    }
-//    @PostMapping("/login-process")
-//    public String processLogin(@RequestParam("email") String email,
-//                               @RequestParam("password") String password, HttpSession session,Model model){
-//        User user = userRepository.findByEmail(email);
-//        if(user!=null){
-//            if(user.getPassword().equals(password)){
-//                if(user.getRole().equals("STUDENT")){
-//                    session.setAttribute("user", user);
-//                    return "redirect:home";
-//                }else{
-//                    session.setAttribute("user", user);
-//                    return "redirect:home";
-//                }
-//            }else {
-//                session.setAttribute("email", email);
-//                return "redirect:login";
-//            }
-//        }else{
-//            return "redirect:login";
-//        }
-//    }
+	@GetMapping("/login")
+	public String login(@RequestParam(value = "error",required = false) String error,@RequestParam(value="logout", required = false) String logout, Model model){
+		if(error!=null) {
+			if(error.equals("true")){
+				model.addAttribute("error","Tài khoản hoặc mật khẩu của bạn không đúng");
+			}
+		}
+		if(logout!=null) {
+			if(logout.equals("success")) {
+				model.addAttribute("logout","Đăng xuất thành công");
+			}
+		}
+		return "login";
+	}
+
 }
