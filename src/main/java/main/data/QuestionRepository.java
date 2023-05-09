ae834b7363aee9d0bf013681343882c983e06006
nyagami <hoangquan05112002@gmail.com>
2023-05-09 14:08:51 +0700

    basic front for exam, add jquery (#25)

--- src/main/java/main/data/QuestionRepository.java
+++ src/main/java/main/data/QuestionRepository.java
@@ -1,10 +1,11 @@
 package main.data;
 
+import main.models.Examination;
 import main.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+    Iterable<Question> findByExamination(Examination examination);
 }
