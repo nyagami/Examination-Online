aff377490f2d8974a024de7eb66d0edf0724a1a1
Nyagami <hoangquan05112002@gmail.com>
2023-05-09 14:07:37 +0700

    basic front for exam, add jquery

--- src/main/java/main/controllers/ExaminationController.java
+++ src/main/java/main/controllers/ExaminationController.java
@@ -12,10 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.text.SimpleDateFormat;
+
 @Controller
 @RequestMapping("/examination")
 public class ExaminationController {
     private final ExaminationRepository examinationRepository;
+    private final QuestionRepository questionRepository;
     private final CourseRepository courseRepository;
     private final ResultRepository resultRepository;
     private final UserRepository userRepository;
@@ -24,13 +27,14 @@ public class ExaminationController {
     @Autowired
     public ExaminationController(ExaminationRepository examinationRepository, ResultRepository resultRepository,
                                  TeacherRepository teacherRepository, StudentRepository studentRepository,
-                                 UserRepository userRepository, CourseRepository courseRepository){
+                                 UserRepository userRepository, CourseRepository courseRepository, QuestionRepository questionRepository){
         this.examinationRepository = examinationRepository;
         this.courseRepository = courseRepository;
         this.resultRepository = resultRepository;
         this.teacherRepository = teacherRepository;
         this.studentRepository = studentRepository;
         this.userRepository = userRepository;
+        this.questionRepository = questionRepository;
     }
 
     @GetMapping("/{id}")
@@ -41,14 +45,20 @@ public class ExaminationController {
         if(user.getRole().equals("TEACHER")){
             Teacher teacher = teacherRepository.findByUser(user);
             if(course.getTeacher().equals(teacher)){
-                model.addAttribute("examination", examination);
+                model.addAttribute("exam", examination);
             }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }else if(user.getRole().equals("STUDENT")){
+        }else if(user.getRole().equals("STUDENT") && examination.getIsVisible()){
             Student student = studentRepository.findByUser(user);
             if(course.getStudentList().contains(student)){
-                model.addAttribute("examination", examination);
+                model.addAttribute("exam", examination);
             }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        model.addAttribute("role", user.getRole());
+        model.addAttribute("startDate", simpleDateFormat.format(examination.getStartDate()));
+        model.addAttribute("endDate", simpleDateFormat.format(examination.getEndDate()));
+        Iterable<Question> questions = questionRepository.findByExamination(examination);
+        model.addAttribute("questions", questions);
         return "examination";
     }
 }
