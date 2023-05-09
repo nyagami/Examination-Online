7666f44bb746ccf97a762206317029a7c5cb1af7
Nyagami <hoangquan05112002@gmail.com>
2023-05-09 20:32:18 +0700

    init exam process

--- src/main/java/main/controllers/ExaminationController.java
+++ src/main/java/main/controllers/ExaminationController.java
@@ -13,28 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/examination")
 public class ExaminationController {
     private final ExaminationRepository examinationRepository;
     private final QuestionRepository questionRepository;
-    private final CourseRepository courseRepository;
     private final ResultRepository resultRepository;
     private final UserRepository userRepository;
     private final TeacherRepository teacherRepository;
     private final StudentRepository studentRepository;
     @Autowired
-    public ExaminationController(ExaminationRepository examinationRepository, ResultRepository resultRepository,
-                                 TeacherRepository teacherRepository, StudentRepository studentRepository,
-                                 UserRepository userRepository, CourseRepository courseRepository, QuestionRepository questionRepository){
+    public ExaminationController(ExaminationRepository examinationRepository, TeacherRepository teacherRepository,
+                                 StudentRepository studentRepository, UserRepository userRepository,
+                                 QuestionRepository questionRepository, ResultRepository resultRepository){
         this.examinationRepository = examinationRepository;
-        this.courseRepository = courseRepository;
-        this.resultRepository = resultRepository;
         this.teacherRepository = teacherRepository;
         this.studentRepository = studentRepository;
         this.userRepository = userRepository;
         this.questionRepository = questionRepository;
+        this.resultRepository = resultRepository;
     }
 
     @GetMapping("/{id}")
@@ -44,21 +43,45 @@ public class ExaminationController {
         Course course = examination.getCourse();
         if(user.getRole().equals("TEACHER")){
             Teacher teacher = teacherRepository.findByUser(user);
-            if(course.getTeacher().equals(teacher)){
-                model.addAttribute("exam", examination);
-            }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
+            if(!course.getTeacher().equals(teacher))
+                throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
         }else if(user.getRole().equals("STUDENT") && examination.getIsVisible()){
             Student student = studentRepository.findByUser(user);
-            if(course.getStudentList().contains(student)){
-                model.addAttribute("exam", examination);
-            }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
+            if(!course.getStudentList().contains(student) || !examination.getIsVisible())
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            Result result = resultRepository.findByExaminationAndStudent(examination, student);
+            if(result != null){
+                Date now = new Date();
+                Long remain = examination.getTotalTime() - (now.getTime() - result.getStartTime().getTime());
+                model.addAttribute("remain", remain);
+            }else{
+                model.addAttribute("remain", examination.getTotalTime());
+            }
         }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        model.addAttribute("exam", examination);
         model.addAttribute("role", user.getRole());
         model.addAttribute("startDate", simpleDateFormat.format(examination.getStartDate()));
         model.addAttribute("endDate", simpleDateFormat.format(examination.getEndDate()));
         Iterable<Question> questions = questionRepository.findByExamination(examination);
         model.addAttribute("questions", questions);
         return "examination";
+    }
+
+    @GetMapping("/{id}/process")
+    public String process(@PathVariable("id") Long id, Model model, Authentication authentication){
+        User user = userRepository.findByUsername(authentication.getName());
+        if(user == null) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        Examination examination = examinationRepository.findById(id).orElseThrow();
+        Student student = studentRepository.findByUser(user);
+        if(!examination.getCourse().getStudentList().contains(student))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+
+/*        Result result = resultRepository.findByExaminationAndStudent(examination, student);
+        if(result == null){
+            result.setExamination(examination);
+            result.setStudent(student);
+        }*/
+        return "process_examination";
     }
 }
