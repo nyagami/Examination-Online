785736960409e06cb93df24af5b09913ed46e3b5
Nyagami <hoangquan05112002@gmail.com>
2023-05-09 12:47:03 +0700

    add boostrap

--- src/main/java/main/controllers/ExaminationController.java
+++ src/main/java/main/controllers/ExaminationController.java
@@ -1,34 +1,54 @@
 package main.controllers;
 
-import main.data.ExaminationRepository;
-import main.data.ResultRepository;
-import main.models.Examination;
-import main.models.Result;
+import main.data.*;
+import main.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/examination")
 public class ExaminationController {
-    private final ExaminationRepository examinationRepo;
-    private final ResultRepository resultRepo;
+    private final ExaminationRepository examinationRepository;
+    private final CourseRepository courseRepository;
+    private final ResultRepository resultRepository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final StudentRepository studentRepository;
     @Autowired
-    public ExaminationController(ExaminationRepository examinationRepo, ResultRepository resultRepo){
-        this.examinationRepo = examinationRepo;
-        this.resultRepo = resultRepo;
+    public ExaminationController(ExaminationRepository examinationRepository, ResultRepository resultRepository,
+                                 TeacherRepository teacherRepository, StudentRepository studentRepository,
+                                 UserRepository userRepository, CourseRepository courseRepository){
+        this.examinationRepository = examinationRepository;
+        this.courseRepository = courseRepository;
+        this.resultRepository = resultRepository;
+        this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/{id}")
-    public String examination(@PathVariable("id") Long id, Model model){
-        System.out.println(id);
-        Examination examination = examinationRepo.getReferenceById(id);
-        Iterable<Result> results = resultRepo.findByExamination(examination);
-        model.addAttribute("examination", examination);
-        model.addAttribute("results", results);
+    public String examination(@PathVariable("id") Long id, Model model, Authentication authentication){
+        User user = userRepository.findByUsername(authentication.getName());
+        Examination examination = examinationRepository.findById(id).orElseThrow();
+        Course course = examination.getCourse();
+        if(user.getRole().equals("TEACHER")){
+            Teacher teacher = teacherRepository.findByUser(user);
+            if(course.getTeacher().equals(teacher)){
+                model.addAttribute("examination", examination);
+            }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }else if(user.getRole().equals("STUDENT")){
+            Student student = studentRepository.findByUser(user);
+            if(course.getStudentList().contains(student)){
+                model.addAttribute("examination", examination);
+            }else throw  new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
         return "examination";
     }
 }
