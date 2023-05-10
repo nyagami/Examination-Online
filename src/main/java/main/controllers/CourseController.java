546d8485fc26940702573574286aecf7af7d7e21
Nyagami <hoangquan05112002@gmail.com>
2023-05-10 10:55:32 +0700

    student course

--- src/main/java/main/controllers/CourseController.java
+++ src/main/java/main/controllers/CourseController.java
@@ -42,6 +42,16 @@ public class CourseController {
 
 	@GetMapping("/student/course/{id}")
 	public String studentCourse(@PathVariable("id") Long id, Model model, Authentication authentication){
+		User user = userRepository.findByUsername(authentication.getName());
+		Student student = studentRepository.findByUser(user);
+		Course course = courseRepository.findById(id).orElseThrow();
+		if(!course.getStudentList().contains(student)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+		Iterable<Student> students = course.getStudentList();
+		Iterable<Examination> examinations = examinationRepository.findByCourseAndIsVisible(course, true);
+		model.addAttribute("students", students);
+		model.addAttribute("exams", examinations);
+		model.addAttribute("teacher", course.getTeacher());
+		model.addAttribute("course", course);
 		return "course_student";
 	}
 }
